658e78ff2ab120845b438102291abfbdb02591ac
Rattus <rattus128@gmail.com>
2025-10-31 13:39:08 +1000

    wip

--- comfy/ops.py
+++ comfy/ops.py
@@ -96,21 +96,24 @@ def cast_bias_weight(s, input=None, dtype=None, device=None, bias_dtype=None, of
 
     bias = None
     non_blocking = comfy.model_management.device_supports_non_blocking(device)
+
+    bias_has_function = len(s.bias_function) > 0
+    weight_has_function = len(s.weight_function) > 0
+
+    weight = comfy.model_management.cast_to(s.weight, dtype, device, non_blocking=non_blocking, copy=weight_has_function, stream=offload_stream)
+
     if s.bias is not None:
-        has_function = len(s.bias_function) > 0
-        bias = comfy.model_management.cast_to(s.bias, bias_dtype, device, non_blocking=non_blocking, copy=has_function, stream=offload_stream)
+        bias = comfy.model_management.cast_to(s.bias, bias_dtype, device, non_blocking=non_blocking, copy=bias_has_function, stream=offload_stream)
 
-        if has_function:
-            with wf_context:
-                for f in s.bias_function:
-                    bias = f(bias)
-
-    has_function = len(s.weight_function) > 0
-    weight = comfy.model_management.cast_to(s.weight, dtype, device, non_blocking=non_blocking, copy=has_function, stream=offload_stream)
-    if has_function:
+    if weight_has_function:
         with wf_context:
             for f in s.weight_function:
                 weight = f(weight)
+
+    if s.bias is not None and bias_has_function:
+        with wf_context:
+            for f in s.bias_function:
+                bias = f(bias)
 
     comfy.model_management.sync_stream(device, offload_stream)
     if offloadable:
